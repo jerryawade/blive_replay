1dcc0bb4508b78ed25d57fe5fee6cfa126c0c318
jerryawade <jerrywade@outlook.com>
2025-03-19 12:44:46 -0500

    Add files via upload

--- view_manual.php
+++ view_manual.php
@@ -14,9 +14,13 @@ date_default_timezone_set($settings['timezone'] ?? 'America/Chicago');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BLIVER RePlay User Manual</title>
+    <title>BLIVE RePlay</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/imgs/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/imgs/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/imgs/favicon-16x16.png">
+    <link rel="manifest" href="/assets/imgs/site.webmanifest">
     <link href="assets/css/styles.css" rel="stylesheet">
     <style>
         body {
