fa0cfa4e00df33dc81a0c383e5b3c38daa50e86f
Bethany <135973397+BethanyIT@users.noreply.github.com>
2025-03-20 22:14:53 -0500

    removed

--- cleanup_messages.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once 'message_actions.php'; // Include functions
-cleanupOldMessages();
--- stayalive 2.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();  // Refreshes the session
-http_response_code(204);  // Returns "No Content" status
-?>
